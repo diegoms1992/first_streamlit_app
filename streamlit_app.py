6f7c7a8bb7e4f7514551e90e90d0303539af86fc
diegoms1992 <144832561+diegoms1992@users.noreply.github.com>
2023-09-12 14:12:03 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents New Healthy Dinner')
 
-streamlit.subtitle('Breakfast Menu:')
+streamlit.title('Breakfast Menu:')
 streamlit.print('\n Omega3 & Blueberry Oatmeal')
 streamlit.print('\n Kale, Spinach & Rocket Smoothfile')
 streamlit.print('\n Omega3 & Blueberry Oatmeal')
