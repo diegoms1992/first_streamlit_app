dedfb8b4be79e37f64a0c110930e072822f1ac65
diegoms1992 <144832561+diegoms1992@users.noreply.github.com>
2023-09-12 22:55:45 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.setIndex('Fruits')
+my_fruit_list = my_fruit_list.set_index('Fruits')
 streamlit.title('My parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu:')
